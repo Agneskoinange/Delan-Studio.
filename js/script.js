cef8b98e53b243a4a46600a64fc9ee52881eefac
Agnes <koinageagness@gmail.com>
2022-03-12 16:42:22 +0300

    add jQuery commands

--- js/script.js
+++ js/script.js
@@ -14,14 +14,27 @@
 //       });
 
 
-$(document).ready(function(){
-    $("mouse").hover(function(){
-      $("#p").hide();
-    });
-  });
+// $(document).ready(function(){
+//     $("mouse").hover(function(){
+//       $("#p").hide();
+//     });
+//   });
 
-  $(document).ready(function(){
-    $("mouse").hover(function(){
-      $("#p").show();
-    });
-  });
+//   $(document).ready(function(){
+//     $("mouse").hover(function(){
+//       $("#p").show();
+//     });
+//   });
+
+// $(document).ready(function(){
+
+//   hover: function( fnOver, fnOut ) {
+//     return this.mouseenter( fnOver ).mouseleave( fnOut || fnOver );
+
+// $(document).ready(function(){
+//     $(".p").hover(function () {
+//     $(this).animate({opacity:'1'});
+//     },
+//     function () {
+//     $(this).animate({opacity:'0'});
+// })
